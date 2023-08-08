21a48fff5a6dc817400b1b46da4a0a1383067324
KTELLAWI <koutaiba@msn.com>
2023-08-08 12:27:01 +0300

    first commit7

--- app.py
+++ app.py
@@ -139,8 +139,8 @@ def main():
     if uploaded_files is not None and data_points is not None:
         results = []
         for file in uploaded_files:
-            with NamedTemporaryFile(dir='.', suffix='.csv') as f:
-                f.write(file.getbuffer())
+            # with NamedTemporaryFile(dir='.', suffix='.csv') as f:
+            #     f.write(file.getbuffer())
                 content = convert_pdf_to_images(file.name)
                 content1= extract_text_from_img(content)
                 # content = extract_content_from_url(f.name)
